fb4ce6ee36d78ae18148b3ec5e546bc5898fabfe
FatmaNasser29 <fatma.nasser1213@gmail.com>
2025-01-29 20:25:30 +0200

    add button function and operator functions

--- calculator/lib/calculator_button.dart
+++ calculator/lib/calculator_button.dart
@@ -2,17 +2,26 @@ import 'package:flutter/material.dart';
 
 class CalculatorButton extends StatelessWidget {
   String label;
-
-  CalculatorButton({required this.label, super.key});
+  Function onClick;
+  CalculatorButton({required this.label, required this.onClick, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: ElevatedButton(
-      onPressed: () {},
-      child: Text(
-        "4",
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape: CircleBorder(),
+            backgroundColor: Colors.grey,
+            foregroundColor: Colors.white),
+        onPressed: () {
+          onClick(label);
+        },
+        child: Text(
+          label,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+        ),
       ),
     ));
 
--- calculator/lib/home.dart
+++ calculator/lib/home.dart
@@ -1,121 +1,204 @@
 import 'package:calculator/calculator_button.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   static const String routeName = "home";
-  const Home({super.key});
+  Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String resultScreen = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Calculator",
-          style: TextStyle(fontWeight: FontWeight.bold),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          // centerTitle: false,
+          title: Text(
+            "Calculator",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Colors.greenAccent),
+          ),
         ),
-      ),
-      body: Container(
-        color: Colors.blue,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-                child: Text(
-              "233444555",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
-            )),
-            Expanded(
-              child: Container(
-                color: Colors.red,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    CalculatorButton(
-                      label: "7",
+        body: Container(
+          color: Colors.black,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      resultScreen,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 50,
+                          color: Colors.white),
                     ),
-                    CalculatorButton(
-                      label: "8",
-                    ),
-                    CalculatorButton(
-                      label: "9",
-                    ),
-                    CalculatorButton(
-                      label: "+",
-                    ),
-                  ],
+                  )),
+              Expanded(
+                child: Container(
+                  color: Colors.black,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CalculatorButton(
+                        label: "7",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "8",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "9",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "/",
+                        onClick: onOperatorClicked,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.red,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    CalculatorButton(
-                      label: "4",
-                    ),
-                    CalculatorButton(
-                      label: "5",
-                    ),
-                    CalculatorButton(
-                      label: "6",
-                    ),
-                    CalculatorButton(
-                      label: "-",
-                    ),
-                  ],
+              Expanded(
+                child: Container(
+                  color: Colors.black,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CalculatorButton(
+                        label: "4",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "5",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "6",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "*",
+                        onClick: onOperatorClicked,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.red,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    CalculatorButton(
-                      label: "1",
-                    ),
-                    CalculatorButton(
-                      label: "2",
-                    ),
-                    CalculatorButton(
-                      label: "3",
-                    ),
-                    CalculatorButton(
-                      label: "/",
-                    ),
-                  ],
+              Expanded(
+                child: Container(
+                  color: Colors.black,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CalculatorButton(
+                        label: "1",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "2",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "3",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "+",
+                        onClick: onOperatorClicked,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.red,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    CalculatorButton(
-                      label: "0",
-                    ),
-                    CalculatorButton(
-                      label: ".",
-                    ),
-                    CalculatorButton(
-                      label: "00",
-                    ),
-                    CalculatorButton(
-                      label: "*",
-                    ),
-                  ],
+              Expanded(
+                child: Container(
+                  color: Colors.black,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CalculatorButton(
+                        label: "0",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: ".",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "=",
+                        onClick: onButtonClicked,
+                      ),
+                      CalculatorButton(
+                        label: "-",
+                        onClick: onOperatorClicked,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  onButtonClicked(label) {
+    resultScreen += label;
+    setState(() {});
+  }
+
+  String op = "";
+  String LHS = "";
+
+  String calculate(String lhs, String op, String rhs) {
+    double LHS = double.parse(lhs);
+    double RHS = double.parse(rhs);
+
+    if (op == "+") {
+      double res = LHS + RHS;
+      return res.toString();
+    } else if (op == "-") {
+      double res = LHS - RHS;
+      return res.toString();
+    } else if (op == "*") {
+      double res = LHS * RHS;
+      return res.toString();
+    } else if (op == "/") {
+      double res = LHS / RHS;
+      return res.toString();
+    } else {
+      return "";
+    }
+  }
+
+  onOperatorClicked(label) {
+    if (op.isEmpty) {
+      LHS = resultScreen;
+      // op = label;
+      // resultScreen = '';
+    } else {
+      resultScreen = calculate(LHS, op, resultScreen);
+      LHS = resultScreen;
+      // op = label;
+      // resultScreen = '';
+    }
+    op = label;
+    resultScreen = '';
+    setState(() {});
+    print(LHS);
   }
 }
